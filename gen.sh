c20855d22c3a3f012cb1db4e3d742b56d4038f56
Geoff Simons <geoff.simons@gmail.com>
2017-01-02 17:22:01 -0800

    Initial commit

---
+++ gen.sh
@@ -0,0 +1,23 @@
+mkdir -p $1
+cp .eslintrc $1/.eslintrc
+cp .gitignore $1/.gitignore
+cp README.md $1/README.md
+cp gulpfile.js $1/gulpfile.js
+cp package.json $1/package.json
+# Possibly: cp .env $1/.env
+
+# change to the new project root
+cd $1
+
+# NOTE: Removing git init because we often
+# are working with empty clones of forks
+# we are supposed to work in.
+# setup git, script assumes new project
+# git init
+
+# Install node dependencies
+npm i
+
+# Consider git add . && git commit -m "Initial commit"
+# Should that happen on a branch, or maybe this is something that
+# can happen on master.
